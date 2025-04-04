d80ed7973db374ab8da03117e59af38908fc8ed3
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2025-04-04 21:29:25 +0200

    add task

--- src/argdown_hirpo/tasks/sequential/arganno_from_argmap.py
+++ src/argdown_hirpo/tasks/sequential/arganno_from_argmap.py
@@ -1,4 +1,7 @@
 from textwrap import dedent
+from typing import Any
+from bs4 import BeautifulSoup
+from pyargdown import ArgdownMultiDiGraph, Valence
 import textdistance
 
 from argdown_hirpo.base import (
@@ -9,30 +12,42 @@ from argdown_hirpo.base import (
     ScoringVirtuePreferencePairGenerator,
     Solution,
 )
-from argdown_hirpo.tasks.core.infreco import (
-    InfRecoProblem,
-    InformalReco
-)
 from argdown_hirpo.tasks.core.arganno import (
-    AnnotationProblemGenerator,
-    AnnotationJudge,
+    Annotation,
+    AnnotationProblem,
+    ANNOTATION_SCHEME,
+)
+from argdown_hirpo.tasks.core.argmap import (
+    ArgMapProblemGenerator,
+    ArgMapJudge
 )
 
 
-class InfRecoFromArgAnnoProblem(InfRecoProblem):
+class ArgannoFromArgmapProblem(AnnotationProblem):
     """
-    Task: Reconstruct the main argument as a premise conclusion structure,
-    no formalization, no dialectics.
-    Input: Source text and argumentative annotation.
+    Task: Annotate a source text given an informal argument map.
+    Input: Source text and argument map.
     """
 
-    def __init__(self, annotation: str):
-        if isinstance(annotation, list):
-            annotation = "\n\n-----\n\n".join(annotation)
-        # remove leading and trailing whitespace and newlines
-        annotation = annotation.strip("\n ")
-        self.annotation = annotation
-        self.sources = annotation
+    def __init__(
+        self,
+        sources: str | list[str],
+        argdown_snippet: str,
+        argdown_map: ArgdownMultiDiGraph | None = None,
+        argmap_evaluation: Evaluation | None = None,
+        strip_html: bool = True,
+    ):
+        if isinstance(sources, list):
+            sources = "\n\n-----\n\n".join(sources)
+        # strip html tags
+        if strip_html:
+            sources = BeautifulSoup(sources, "html.parser").get_text()
+        # remove leading and trailing whitespace
+        sources = sources.strip()
+        self.sources = sources
+        self.argdown_snippet = argdown_snippet
+        self.argdown_map = argdown_map
+        self.argmap_evaluation = argmap_evaluation
 
     def instruct_prompt(
         self,
@@ -40,41 +55,36 @@ class InfRecoFromArgAnnoProblem(InfRecoProblem):
         hints: list[str] | None = None,
         evaluation: Evaluation | None = None,
     ) -> str:
+        qualifier = "(arguably imperfect) " if self.argmap_evaluation and not self.argmap_evaluation.is_valid else ""
         prompt = (
             dedent("""
-            Assignment: Reconstruct a source text's main argument in standard form.
+            Assignment: Apply a given annotation scheme to a source text.
                         
-            Use the argumentative annotation to identify the main argument in the following source text and reconstruct it as premise-conclusion structure using Argdown.
+            Annotate the following **source text** in order to identify the argumentative function of different parts in the text.
 
             ::: {{.source_text}}              
             {sources}
             :::
 
-            Note in particular:
+            This {qualifier}argument map sketches the source text's argumentative structure:
 
-            - Enclose your Argdown argument reconstruction in a fenced codeblock, starting with '```argdown' and
-              ending with '```'. Just include a single Argdown codeblock in your answer.                                            
-            - In your Argdown snippet, only reconstruct *a single argument* in standard form (including premises, final 
-              conclusion, and possible intermediate conclusions), no matter whether the annotation highlights more than
-              one argument.
-            - For each conclusion in the argument, provide information about which previously introduced premises or 
-              conclusions it is inferred *from*, using yaml inline data in the inference line, e.g. `-- {{'from': ['1','3']}} --`,
-              where the list items refer to the respective premise or conclusion labels.
-            - You may, but are in no way required to add additional information about which inference rules or argumentation
-              schemes are applied in each sub-argument.
-            - In addition, at the beginning of your Argdown code block, provide a succinct label (title) for the argument and 
-              summarize its gist in line with Argdown syntax conventions. 
-                   
-            Carefully consider the following DON'Ts:
+            {argdown_snippet}
+                                      
+            Annotate the source text above according to the following schema:
 
-            - Do NOT include any other analyses (maps or arguments) in your Argdown snippet besides the reconstruction of the main argument.
-            - Do NOT add any inline dialectical relations in the premise conclusion structure.
-            - Do NOT add any yaml inline data besides the required inference information.
-            - Do NOT add any formalization of the argument's propositions (premises or conclusions) in your Argdown code.
+            {annotation_scheme}
 
-        """)
-            .strip()
-            .format(sources=self.annotation)
+            In particular:           
+        
+            1. Just add tags and attributes to the source text to mark the argumentative function of each part. Don't modify the text in any other way.                        
+            2. Use the `argument_label` attribute to relate the annotated text segments to the given informal argument map.
+            3. Enclose the annotated text in a single fenced codeblock, starting with '```xml' and ending with '```'.
+            """)
+        ).strip().format(
+            sources=self.sources,
+            argdown_snippet=self.argdown_snippet,
+            annotation_scheme=ANNOTATION_SCHEME,
+            qualifier=qualifier,
         )
 
         if hints:
@@ -97,55 +107,141 @@ class InfRecoFromArgAnnoProblem(InfRecoProblem):
         return prompt
 
 
-class InfRecoFromArgAnnoProblemGenerator(ProblemGeneratorLLM):
+class ArgannoFromArgmapProblemGenerator(ProblemGeneratorLLM):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._arganno_pg = AnnotationProblemGenerator()
-        self._arganno_sg = GenericSolutionGenerator(*args, **kwargs)
+        self._argmap_pg = ArgMapProblemGenerator()        
+        self._argmap_sg = GenericSolutionGenerator(*args, **kwargs, n_solutions=1)
 
     async def arun(self, inputs) -> Problem:
         if isinstance(inputs, str) or (
             isinstance(inputs, list) and all(isinstance(i, str) for i in inputs)
         ):
-            arganno_problem = await self._arganno_pg.arun(inputs)
-            arganno_solution = await self._arganno_sg.arun(arganno_problem)
-            return InfRecoFromArgAnnoProblem(str(arganno_solution))
+            argmap_problem = await self._argmap_pg.arun(inputs)
+            argmap_solution = await self._argmap_sg.arun(argmap_problem)
+            argmap_evaluation = ArgMapJudge()._evaluate_argmap(argmap_problem, argmap_solution[0])
+            argdown_map = argmap_evaluation.artifacts.get("argdown_map")
+            return ArgannoFromArgmapProblem(
+                sources=inputs,
+                argdown_snippet=str(argmap_solution),
+                argdown_map=argdown_map,
+                argmap_evaluation=argmap_evaluation,
+            )
         raise ValueError(
-            "Inputs to an argument reconstruction problem must be a string or a list of strings"
+            "Inputs to an ArgannoFromArgmapProblem must be a string or a list of strings"
         )
 
 
-class AnnotationProximityPreferencePairGenerator(ScoringVirtuePreferencePairGenerator):
-    """Generate virtue-preference pairs for the argument reco task, prefering valid argument recos
-    that stick closely to the source text's annotation."""
+class ArgmapTextProximityPreferencePairGenerator(ScoringVirtuePreferencePairGenerator):
+    """Generate virtue-preference pairs for the annotation task, prefering valid annotations
+    that succeed in sticking closely to the informal argument map."""
 
     hints = [
-        "Make sure that your argument reconstruction stays faithful to and mimics closely "
-        "the annotation of the source text. In particular, try to use supporting propositions from the annotation "
-        "as premises / intermediate conclusions in your argument reconstruction!"
+        "Make sure that your annotation of the source text mimics closely "
+        "the informal argument map in terms of text flow and wording. "
+        "In particular, try to annotate propositions segments "
+        "that, taken together, match the argument map!"
     ]
 
     def _score(
         self,
         problem: Problem,
-        reco: Solution,
+        anno: Solution,
         evaluation: Evaluation,
     ) -> float:
-        assert isinstance(problem, InfRecoFromArgAnnoProblem)
-        assert isinstance(reco, InformalReco)
+        assert isinstance(problem, ArgannoFromArgmapProblem)
+        assert isinstance(anno, Annotation)
 
-        soup, _ = AnnotationJudge().parse_xml_snippet(problem.annotation)
+        soup = evaluation.artifacts.get("soup")
+        if soup is None:
+            return 0
         anno_props = soup.find_all("proposition")
-        supporting_props = [
-            ap
-            for ap in anno_props
-            if ap.get("supports")  # type: ignore
-        ]
-        list_anno_props = "\n".join([ap.text for ap in supporting_props])
+        list_anno_props = "\n".join([ap.text for ap in anno_props])
 
         return round(
             textdistance.damerau_levenshtein.normalized_similarity(
-                list_anno_props, reco.argdown_snippet
+                list_anno_props, problem.argdown_snippet
             ),
             1,
         )
+
+
+
+class ArgmapGraphProximityPreferencePairGenerator(ScoringVirtuePreferencePairGenerator):
+    """Generate virtue-preference pairs for the annotation task, prefering valid annotations
+    that are structurally similar to the informal argument map."""
+
+    hints = [
+        "Make sure that your annotation of the source text stays faithful to and mimics closely "
+        "the informal argument map in terms of overall argumentative structure. "
+        "In particular, match arguments and claims via `argument_label` references, and "
+        "reproduce the sketched dialectic relations in your annotation!"
+    ]
+
+    def _score(
+        self,
+        problem: Problem,
+        anno: Solution,
+        evaluation: Evaluation,
+    ) -> float:
+        assert isinstance(problem, ArgannoFromArgmapProblem)
+        assert isinstance(anno, Annotation)
+
+        argdown_map = problem.argdown_map
+        soup = evaluation.artifacts.get("soup")
+        if argdown_map is None or soup is None:
+            return 0
+        anno_props = soup.find_all("proposition")
+
+        supports_anno: list[tuple[Any, Any]] = []
+        attacks_anno: list[tuple[Any, Any]] = []
+        for ap in anno_props:
+            from_id = ap.get("id")  # type: ignore
+            if from_id is None:
+                continue
+            for to_id in ap.get("supports", []):  # type: ignore
+                if to_id is None:
+                    continue
+                supports_anno.append((ap["id"], to_id))  # type: ignore
+            for to_id in ap.get("attacks", []):  # type: ignore
+                if to_id is None:
+                    continue
+                attacks_anno.append((ap["id"], to_id))  # type: ignore
+
+        # helper fn
+        
+        def arg_label(anno_id: str) -> str | None:
+            if not anno_id:
+                return None
+            ap = next((a for a in anno_props if a.get("id") == anno_id), None)  # type: ignore
+            if not ap:
+                return None
+            return ap.get("argument_label") # type: ignore
+
+
+        matched_n = 0
+        for drel in argdown_map.dialectical_relations:
+            if any(
+                drel.valence == Valence.SUPPORT
+                and drel.source == arg_label(from_id)
+                and drel.target == arg_label(to_id)
+                for from_id, to_id in supports_anno                
+            ):
+                matched_n += 1
+
+        for drel in argdown_map.dialectical_relations:
+            if any(
+                drel.valence == Valence.ATTACK
+                and drel.source == arg_label(from_id)
+                and drel.target == arg_label(to_id)
+                for from_id, to_id in attacks_anno                
+            ):
+                matched_n += 1
+
+        print("MATCHED_N", matched_n)
+
+        return round(
+            matched_n / len(argdown_map.dialectical_relations),
+            1,
+        )
+
